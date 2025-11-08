ee1ae85ce1a637e67bb78bb681cc6553b996db9a
sith91 <sithijag@gmail.com>
2025-11-08 11:06:04 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'bounce-slow': 'bounce 2s infinite',
+      },
+      backdropBlur: {
+        xs: '2px',
+      },
+      colors: {
+        slate: {
+          850: '#1e293b',
+          950: '#0f172a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
