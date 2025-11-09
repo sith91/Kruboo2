5192ab274b252eeb62235e6b87841681b8c1cbe9
sith91 <sithijag@gmail.com>
2025-11-09 13:50:17 +0530

    Create __init__.py

---
+++ backend/src/blockchain/__init__.py
@@ -0,0 +1,5 @@
+from .did_manager import DIDManager
+from .wallet_integration import WalletIntegration
+from .decentralized_storage import DecentralizedStorage
+
+__all__ = ["DIDManager", "WalletIntegration", "DecentralizedStorage"]
