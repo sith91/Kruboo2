5e72304e56d56c57dd8ba6f3f09cc4f74515c16e
sith91 <sithijag@gmail.com>
2025-11-09 10:56:50 +0530

    Create __init__.py

---
+++ backend/src/system/__init__.py
@@ -0,0 +1,6 @@
+from .automation_engine import AutomationEngine
+from .file_manager import FileManager
+from .app_controller import AppController
+from .system_monitor import SystemMonitor
+
+__all__ = ["AutomationEngine", "FileManager", "AppController", "SystemMonitor"]
