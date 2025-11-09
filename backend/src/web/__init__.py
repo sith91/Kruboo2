f2246e30454b20f58c1d336d9528dec576814e25
sith91 <sithijag@gmail.com>
2025-11-09 11:00:56 +0530

    Create __init__.py

---
+++ backend/src/web/__init__.py
@@ -0,0 +1,5 @@
+from .search_engine import SearchEngine
+from .browser_automation import BrowserAutomation
+from .research_assistant import ResearchAssistant
+
+__all__ = ["SearchEngine", "BrowserAutomation", "ResearchAssistant"]
